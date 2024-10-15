096a9515850a9ffaeae38284d744ba4704161b8f
Misael Juarez <138533025+MisaelJuarez@users.noreply.github.com>
2024-10-14 21:15:34 -0600

    Correccion de los correos repetidos

--- app/controller/actualizar_info_usuario.php
+++ app/controller/actualizar_info_usuario.php
@@ -2,57 +2,75 @@
 session_start();
 require_once '../config/conexion.php';
 
-if (!empty($_POST['nombre']) && !empty($_POST['apellido']) && !empty($_POST['email']) && !empty($_POST['pass'])) {
+$expresion = '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/';
 
-    $nombre = $_POST['nombre'];
-    $apellido = $_POST['apellido'];
-    $email = $_POST['email'];
-    $pass= $_POST['pass'];
+function comprobar_emails($datos,$email) {
+    foreach($datos as $emails) {
+        if ($emails['email'] == $email) {
+            return true;
+        } 
+    }
+}
 
-    if (!is_numeric($nombre) && !is_numeric($apellido)) {
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$email = $_POST['email'];
+$pass= $_POST['pass'];
+
+if (empty($_POST['nombre']) && empty($_POST['apellido']) && empty($_POST['email']) && empty($_POST['pass'])) {
+    echo json_encode([0,"Campos incompletos"]);
+} else if(is_numeric($nombre) || is_numeric($apellido)) {
+    echo json_encode([0,"No puedes ingresar numeros en nombre y apellido"]);
+} else if (!preg_match($expresion,$_POST['email'])) {
+    echo json_encode([0,"No cumples con las especificaciones de un correo"]);
+} else {
+
+    if ($_SESSION['usuario']['email'] != $email || $_SESSION['usuario']['pass'] != $pass) {
+
+        $usuariosEmail = $conexion->prepare("SELECT email FROM t_usuarios WHERE id_usuario != :id_usuario");
+        $usuariosEmail->bindParam(':id_usuario',$_SESSION['usuario']['id_usuario']);
+        $usuariosEmail->execute();
+        $emails = $usuariosEmail->fetchAll(PDO::FETCH_ASSOC);
+
+        if (comprobar_emails($emails,$email)) {
+            echo json_encode([0,"Ese correo ya existe, ingrese otro correo"]);
+        } else {
+            $actualizacion = $conexion->prepare("UPDATE t_usuarios 
+            SET nombre = :nombre, apellido = :apellido, email = :email, pass = :pass  
+            WHERE id_usuario = :id_usuario");
+
+            $actualizacion->bindParam(':nombre',$nombre);
+            $actualizacion->bindParam(':apellido',$apellido);
+            $actualizacion->bindParam(':email',$email);
+            $actualizacion->bindParam(':pass',$pass);
+            $actualizacion->bindParam(':id_usuario',$_SESSION['usuario']['id_usuario']);
+
+            $actualizacion->execute();
+
+            echo json_encode(["cerrar","Actualizacion correcta","Tu session se cerrara para que ingreses de nuevo tus datos"]);
+        }
+    } else {
         $actualizacion = $conexion->prepare("UPDATE t_usuarios 
             SET nombre = :nombre, apellido = :apellido, email = :email, pass = :pass  
             WHERE id_usuario = :id_usuario");
 
-        $actualizacion->bindParam(':nombre',$nombre);
-        $actualizacion->bindParam(':apellido',$apellido);
-        $actualizacion->bindParam(':email',$email);
-        $actualizacion->bindParam(':pass',$pass);
-        $actualizacion->bindParam(':id_usuario',$_SESSION['usuario']['id_usuario']);
+            $actualizacion->bindParam(':nombre',$nombre);
+            $actualizacion->bindParam(':apellido',$apellido);
+            $actualizacion->bindParam(':email',$email);
+            $actualizacion->bindParam(':pass',$pass);
+            $actualizacion->bindParam(':id_usuario',$_SESSION['usuario']['id_usuario']);
 
-        $actualizacion->execute();
+            $actualizacion->execute();
 
-        if ($actualizacion) {
             $consulta = $conexion->prepare("SELECT * FROM t_usuarios WHERE id_usuario = :id_usuario");
             $consulta->bindParam(':id_usuario',$_SESSION['usuario']['id_usuario']);
             $consulta->execute();
             $datos = $consulta->fetch(PDO::FETCH_ASSOC);
 
-            if ($datos) {
-                if ($_SESSION['usuario']['email'] != $email || $_SESSION['usuario']['pass'] != $pass) {
-                    echo json_encode(["cerrar","Actualizacion correcta","Tu session se cerrara para que ingreses de nuevo tus datos"]);
-                } else {
-                    $_SESSION['usuario'] = $datos;
-                    echo json_encode([1,"Informacion actualizada correctamente"]);
-                }
-
-            } else {
-                echo json_encode([0,"Error al actualizar datos"]);
-            }
-        
-        } else {
-            echo json_encode([0,"Error al actualizar datos"]);
-        }
-    }else {
-        echo json_encode([0,"No puedes ingresar numeros en nombre y apellido"]);
+            $_SESSION['usuario'] = $datos;
+            echo json_encode([1,"Informacion actualizada correctamente"]);
     }
-} else {
-    echo json_encode([0,"Campos incompletos"]);
+
 }
-
-
-
-
-
 
 ?>
--- app/controller/registro.php
+++ app/controller/registro.php
@@ -4,50 +4,61 @@ session_start();
 
 $expresion = '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/';
 
-if (isset($_SESSION['usuario'])) {
-    header("location: ./index.php");
+function comprobar_emails($datos,$email) {
+    foreach($datos as $emails) {
+        if ($emails['email'] == $email) {
+            return true;
+        } 
+    }
 }
 
-if ($_POST) {
-    if (isset($_POST['nombre']) && !empty($_POST['nombre']) && 
-        isset($_POST['apellido']) && !empty($_POST['apellido']) &&
-        isset($_POST['email']) && !empty($_POST['email']) &&
-        isset($_POST['pass']) && !empty($_POST['pass'])) {
+if (isset($_POST['nombre']) && !empty($_POST['nombre']) && 
+    isset($_POST['apellido']) && !empty($_POST['apellido']) &&
+    isset($_POST['email']) && !empty($_POST['email']) &&
+    isset($_POST['pass']) && !empty($_POST['pass'])) {
 
-        if(is_numeric($_POST['nombre'])) {
-            echo json_encode([0,"No puedes agregar numeros en el input nombre"]);
-        } else if(is_numeric($_POST['apellido'])) {
-            echo json_encode([0,"No puedes agregar numeros en el input apellido"]);
-        } else if (!preg_match($expresion,$_POST['email'])) {
-            echo json_encode([0,"No cumples con las especificaciones de un correo"]);
+    if(is_numeric($_POST['nombre'])) {
+        echo json_encode([0,"No puedes agregar numeros en el input nombre"]);
+    } else if(is_numeric($_POST['apellido'])) {
+        echo json_encode([0,"No puedes agregar numeros en el input apellido"]);
+    } else if (!preg_match($expresion,$_POST['email'])) {
+        echo json_encode([0,"No cumples con las especificaciones de un correo"]);
+    } else {
+
+        $nombre = $_POST['nombre'];
+        $apellido = $_POST['apellido'];
+        $email = $_POST['email'];
+        $passw = $_POST['pass'];
+
+        $consulta = $conexion->prepare("SELECT email FROM t_usuarios");
+        $consulta->execute();
+        $datos = $consulta->fetchAll(PDO::FETCH_ASSOC);
+
+        if (comprobar_emails($datos,$email)) {
+            echo json_encode([0,"Ese correo ya existe, ingrese otro correo"]);
         } else {
-
-            $nombre = $_POST['nombre'];
-            $apellido = $_POST['apellido'];
-            $email = $_POST['email'];
-            $passw = $_POST['pass'];
-
             $insercion = $conexion->prepare("INSERT INTO t_usuarios (nombre,apellido,email,pass) 
-                                         VALUES(:nombre,:apellido,:email,:pass)");
+                                            VALUES(:nombre,:apellido,:email,:pass)");
             
             $insercion->bindParam(':nombre',$nombre);
             $insercion->bindParam(':apellido',$apellido);
             $insercion->bindParam(':email',$email);
             $insercion->bindParam(':pass',$passw);
-
+    
             $insercion->execute();
-
+    
             if ($insercion) {
                 echo json_encode([1,"Usuario registrado correctamente"]);
             } else {
                 echo json_encode([0,"Usuario NO registrado"]);
             }
         }
-        
-    } else {
-        echo json_encode([0,"No puedes dejar campos vacios"]);
     }
+    
+} else {
+    echo json_encode([0,"No puedes dejar campos vacios"]);
 }
+
 
 
 
